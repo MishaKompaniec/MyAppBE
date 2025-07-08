aaea07cf18ae2c93e4858f5e275a3e5b7ca3f3ce
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-08 11:58:23 +0200

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import productRoutes from './products/products.controller.js'
 import userRoutes from './users/user.controller.js'
 
 export function createApp() {
   const app = express()
+
+  app.use(cors({
+    origin: 'http://localhost:5173',
+  }))
 
   app.use(express.json())
 
