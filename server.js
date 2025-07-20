73652b558feea45c4ab2965dfc99ba00cb0278ef
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-20 13:40:37 +0200

    remove test

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ dotenv.config()
 const PORT = process.env.PORT || 4200
 const app = createApp()
 
-app.get('/', (req, res) => {
-  res.send('✅ Backend is working!');
-});
-
 connectToDB()
   .then(() => {
     app.listen(PORT, () => {
