8fe91e7f6e5ab47ed2045ce211638d737ed96ce2
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-20 14:17:18 +0200

    change cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ export function createApp() {
   const app = express()
 
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://flower-shop-fe.vercel.app'],
   }))
   app.use(express.json())
   app.use('/api/products', productRoutes)
