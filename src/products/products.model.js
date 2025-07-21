7cd66c1f005c441a8cefc5db0b0ce533f55c81fe
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-21 12:59:14 +0200

    fix description validation

--- src/products/products.model.js
+++ src/products/products.model.js
@@ -5,7 +5,7 @@ const productsItemSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true, default: () => uuidv4() },
   title: { type: String, required: true, maxlength: [30, 'Title must be at most 30 characters'] },
   price: { type: Number, required: true, max: [100000, 'Price must be at most 100000'] },
-  description: { type: String, maxlength: [50, 'Description must be at most 50 characters'] },
+  description: { type: String, maxlength: [80, 'Description must be at most 80 characters'] },
   category: {
     type: String,
     enum: {
